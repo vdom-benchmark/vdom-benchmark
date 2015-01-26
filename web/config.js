3185697313953f43eb78c7ff823318a6bd854ffd
Boris Kaul <localvoid@gmail.com>
2015-01-26 21:08:03 +0600

    Fix link to tests

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 benchmarkConfig({
-  "tests": "http://localhost:3000/tests.js",
+  "tests": "http://vdom-benchmark.github.io/vdom-benchmark/tests.js",
   "contestants": [
     {
       "name": "kivi",
@@ -33,46 +33,3 @@ benchmarkConfig({
     }
   ]
 });
-/*
-benchmarkConfig({
-  "name": "VDom Benchmark",
-  "description": "Comparing performance of the diff/patch operations in various virtual dom libraries.",
-  "data": {
-    "type": "script",
-    "url": "http://vdom-benchmark.github.io/vdom-benchmark/generator.js"
-  },
-  "contestants": [
-    {
-      "name": "kivi",
-      "url": "https://github.com/localvoid/kivi",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-kivi/"
-    },
-    {
-      "name": "cito.js",
-      "url": "https://github.com/joelrich/citojs",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-cito/"
-    },
-    {
-      "name": "Bobril",
-      "url": "https://github.com/Bobris/Bobril",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-bobril/"
-    },
-    {
-      "name": "React",
-      "url": "http://facebook.github.io/react/",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-react/"
-    },
-    {
-      "name": "virtual-dom",
-      "url": "https://github.com/Matt-Esch/virtual-dom",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-virtual-dom/"
-    },
-    {
-      "name": "mithril",
-      "url": "http://lhorie.github.io/mithril/",
-      "benchmarkUrl": "http://vdom-benchmark.github.io/vdom-benchmark-mithril/"
-    }
-  ]
-});
-
-*/
