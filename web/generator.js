11b80def27fb263d0c0c3e40389080cb3592f347
Boris Kaul <localvoid@gmail.com>
2015-01-26 11:41:47 +0600

    Add more test cases

--- web/generator.js
+++ web/generator.js
@@ -8,34 +8,50 @@ var g = new Generator();
 g.addGroup([500], [
   [transformers.reverse],
   [transformers.shuffle],
-  [transformers.insertFirst],
-  [transformers.insertLast],
-  [transformers.removeFirst],
-  [transformers.removeLast],
-  [transformers.moveFromEndToStart],
-  [transformers.moveFromStartToEnd]
+  [transformers.insertFirst(1)],
+  [transformers.insertLast(1)],
+  [transformers.removeFirst(1)],
+  [transformers.removeLast(1)],
+  [transformers.moveFromEndToStart(1)],
+  [transformers.moveFromStartToEnd(1)],
+  [transformers.removeLast(500)],
+  [transformers.removeFirst(250)],
+  [transformers.removeLast(250)]
+]);
+
+g.addGroup([250], [
+  [transformers.insertFirst(250)],
+  [transformers.insertLast(250)]
 ]);
 
 g.addGroup([50, 10], [
   [transformers.reverse, transformers.skip],
   [transformers.shuffle, transformers.skip],
-  [transformers.insertFirst, transformers.skip],
-  [transformers.insertLast, transformers.skip],
-  [transformers.removeFirst, transformers.skip],
-  [transformers.removeLast, transformers.skip],
-  [transformers.moveFromEndToStart, transformers.skip],
-  [transformers.moveFromStartToEnd, transformers.skip]
+  [transformers.insertFirst(1), transformers.skip],
+  [transformers.insertLast(1), transformers.skip],
+  [transformers.removeFirst(1), transformers.skip],
+  [transformers.removeLast(1), transformers.skip],
+  [transformers.moveFromEndToStart(1), transformers.skip],
+  [transformers.moveFromStartToEnd(1), transformers.skip]
 ]);
 
 g.addGroup([5, 100], [
   [transformers.reverse, transformers.skip],
   [transformers.shuffle, transformers.skip],
-  [transformers.insertFirst, transformers.skip],
-  [transformers.insertLast, transformers.skip],
-  [transformers.removeFirst, transformers.skip],
-  [transformers.removeLast, transformers.skip],
-  [transformers.moveFromEndToStart, transformers.skip],
-  [transformers.moveFromStartToEnd, transformers.skip]
+  [transformers.insertFirst(1), transformers.skip],
+  [transformers.insertLast(1), transformers.skip],
+  [transformers.removeFirst(1), transformers.skip],
+  [transformers.removeLast(1), transformers.skip],
+  [transformers.moveFromEndToStart(1), transformers.skip],
+  [transformers.moveFromStartToEnd(1), transformers.skip]
+]);
+
+g.addGroup([50, 0], [
+  [transformers.skip, transformers.insertLast(10)]
+]);
+
+g.addGroup([5, 0], [
+  [transformers.skip, transformers.insertLast(100)]
 ]);
 
 window.generateBenchmarkData = function(config) {
